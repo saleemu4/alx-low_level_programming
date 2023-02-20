fc78e3a8c33b98217ed385ec4861e550c12a6f48
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-19 16:54:10 -0800

    print alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	char ch = 'a';
-	
-	while(ch <= 'z')
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
 	putchar('\n');
-	
 	return (0);
 }
