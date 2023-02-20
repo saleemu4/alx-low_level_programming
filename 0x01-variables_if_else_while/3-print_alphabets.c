debaeef06071eec5cc485e155ce95d71ef609202
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-19 17:03:55 -0800

    alphABETs

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char ch = 'a';
+
+        while (ch <= 'z')
+        {
+                putchar(ch);
+                ch++;
+        }
+
+	ch = 'A';
+	
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+        putchar('\n');
+        return (0);
+}
