54354212436681245e6f7f77a3099e697ab68d10
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-20 06:08:16 -0800

    yet another comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,11 +9,9 @@ int main(void)
 {
 	int i, j;
 
-	i = 0;
-	while (i <= 98)
+	for (i = 0; i <= 98; i++)
 	{
-		j = i + 1;
-		while (j <= 99)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
@@ -21,15 +19,12 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
-			if(i == 98 && j == 99)
+			if (i == 98 && j == 99)
 				continue;
 
 			putchar(',');
 			putchar(' ');
-
-			j++;
 		}
-		i++;
 	}
 	putchar('\n');
 
