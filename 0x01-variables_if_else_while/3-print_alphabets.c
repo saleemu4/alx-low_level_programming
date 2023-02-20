5d3ed820d5840ec4f6f3cc9427302c589a9f45b8
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-19 17:08:54 -0800

    alphABETs

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,20 +8,21 @@
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-	
+
 	ch = 'A';
-	
+
 	while (ch <= 'Z')
 	{
 		putchar(ch);
 		ch++;
 	}
-	
+
 	putchar('\n');
 	return (0);
 }
