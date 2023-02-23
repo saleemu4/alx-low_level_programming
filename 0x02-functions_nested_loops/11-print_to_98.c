01301c2da2af19c036224655b75dbfe497d05b45
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-22 20:29:18 -0800

    Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,15 +9,24 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
-		if (i == 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
+			if (i == 98)
+				printf("%d", i);
+			else
+				printf("%d, ", i);
 		}
-		else
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			if (i == 98)
+				printf("%d", i);
+			else
+				printf("%d, ", i);
 		}
 	}
 	printf("\n");
