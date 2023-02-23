d84417e2c43334b58c59c2338038d4bbad116f12
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-22 19:50:27 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,14 +13,15 @@ void times_table(void)
 
 		for (j = 0; j <= 9; j++)
 		{
-			if ((i * j) > 9 )
+			if ((i * j) > 9)
 				_putchar(((i * j) / 10) + '0');
-			else
+			else if (j != 0)
 				_putchar(' ');
 
 			_putchar('0' + ((i * j) % 10));
 
-			if (j != 9){
+			if (j != 9)
+			{
 				_putchar(',');
 				_putchar(' ');
 			}
