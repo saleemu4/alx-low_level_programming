01269978214810d41cdb4d4c3938479829ec62e1
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-22 18:17:08 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value of int
+ * @n: character to be check
+ * Return: -n if < 0, n otherwise.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
