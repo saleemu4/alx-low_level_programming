7b84fcbf9b93eee25e1247b649f2cc19cb089365
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-22 22:26:51 -0800

    fibonacci sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int sum = 0;
+	long int n1 = 0;
+	long int n2 = 1;
+	long int n3;
+
+	for (;;)
+	{
+		n3 =  n2 + n1;
+		sum += n3;
+
+		if (n3 > 4000000)
+			break;
+
+		n1 = n2;
+		n2 = n3;
+	}
+
+	printf("%li\n", sum);
+	return (0);
+}
