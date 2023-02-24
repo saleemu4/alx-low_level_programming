ee47284c58b8292da060869eebfc295a3ea81e5c
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-23 17:33:54 -0800

    remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -30,8 +30,10 @@ void print_remaining_days(int month, int day, int year)
 
 		/*	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);*/
 		}
-	/*	else
-		{*/
+		else
+		{
+			day++;
+		}
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		/*}*/
