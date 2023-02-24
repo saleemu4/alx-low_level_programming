b6865447d7347e2d4ed8fd0b1f395ee9e535f758
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-23 17:36:40 -0800

    remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -32,7 +32,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			day++;
+			day = day + 1;
 		}
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
