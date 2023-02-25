e1e7105b7683ee0b212fd12809fd0d0f44eec8b0
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-24 19:32:32 -0800

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	
+	i = 1;
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
