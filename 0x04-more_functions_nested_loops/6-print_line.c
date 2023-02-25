8d3e9c5c2e80eccb31020737f091acd6e53585f3
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-24 18:14:19 -0800

    draw lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line n times _.
+ * @n: number of times _ should be printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0){
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
