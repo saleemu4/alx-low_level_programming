79f585e6bf13a03dfcd8c8e5139ca181c0d72a9a
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-24 20:22:57 -0800

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,14 +2,13 @@
 
 /**
  * main - Prints the largest prime factor of 612852475143
- *
  * Return: Always 0.
  */
 int main(void)
 {
 	long i;
 	long num = 612852475143;
-	
+
 	i = 2;
 	while (i < (num / 2))
 	{
