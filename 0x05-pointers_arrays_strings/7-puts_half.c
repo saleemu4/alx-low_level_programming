a5160232f525638c8a60a5a4fcce49e0559ca541
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-28 14:21:30 -0800

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ void puts_half(char *str)
 	if ((len % 2) == 0)
 	{
 		n = len / 2;
-		for (i = n; i <= len; i++)
+		for (i = n; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
@@ -25,7 +25,7 @@ void puts_half(char *str)
 	else
 	{
 		n = (len - 1) / 2;
-		for (i = n; i <= len; i++)
+		for (i = n; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
