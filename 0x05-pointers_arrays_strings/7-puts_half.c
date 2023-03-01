8b49d2bc5e3e4102e5941d3184991588f3dcffb2
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-02-28 16:34:59 -0800

    print half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,20 +15,11 @@ void puts_half(char *str)
 	str = str - len;
 
 	if ((len % 2) == 0)
-	{
 		n = len / 2;
-		for (i = n; i < len; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
 	else
-	{
 		n = (len - 1) / 2;
-		for (i = n; i < len; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
