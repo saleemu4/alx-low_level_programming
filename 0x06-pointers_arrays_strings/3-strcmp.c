9bdad031db88f342e5992b3594f92e5a7caf12b2
saleemu4 <muhdsalim@yumsuk.edu.ng>
2023-03-03 16:22:03 -0800

    compare string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - copares two strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ *
+ * Return: 0 if s1 == s2, positive diff. of chars unmatched if s1>s2
+ * neagetiv diff. of chars unmatched if s1<s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	while ((*s1 && s2) && (s1 == s2))
+	{
+		s1++;
+		s2++;
+	}
+	result = *s1 - *s2;
+
+	return (result);
+}
